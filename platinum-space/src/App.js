fa9834203e7f964d4a8482d5708ee50d85f61f1c
cristian-rios <cristian.rios@moxit.net>
2020-11-04 02:49:12 -0300

    updated project

--- platinum-space/src/App.js
+++ platinum-space/src/App.js
@@ -1,25 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactPlayer from "react-player";
 
 function App() {
+  console.cowsay('hi im cris :D')
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div
+        style={{
+          position: "absolute",
+          height: "100%",
+          width: "100%",
+          textAlign: "center",
+          backgroundColor: "black",
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+          overflowY: "hidden",
+        }}
+      >
+        <ReactPlayer
+          url="https://www.youtube.com/watch?v=X1M69l7ZGlw&t=2s"
+          playing
+          loop
+          height="100%"
+          width="100%"
+        />
+      </div>
+    </>
   );
 }
 
